eb0735750ea9bd91eafb0a377672bd1da15dcc7b
SteveImmanuel <steveandreasimanuel@gmail.com>
2022-05-10 23:45:39 +0900

    Add comment for state and action space

--- policy.py
+++ policy.py
@@ -1,5 +1,8 @@
 from sklearn import svm
 
+# state xa ya za ha la wa cos sin hb wb hc wc
+# action px py pz d1x d1y d1z d2x d2y d2z d3x d3y d3z f pre spt1 spt2 spt3
+
 X = [[0, 0, 1, 4, 3, 12, 43], [1, 1, 5, 23, 54, 12, 5]]
 y = [0, 1]
 clf = svm.SVC()
